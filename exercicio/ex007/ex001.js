72687e4fbceb2bb1696b1dd14312e195c7ff458d
pudin de banha <micapadua@hotmail.com>
2025-04-06 15:11:00 -0300

    Initial commit

---
+++ exercicio/ex007/ex001.js
@@ -0,0 +1,18 @@
+var c = 1
+do {
+    console.log(`Contagem ${c}`)
+    c++
+} while (c <= 6)
+
+
+
+
+
+/* 
+Teste de laço repetições simples em JS
+var c = 1
+while (c <= 10) {
+    console.log(`Contagem ${c}`)
+    c++
+}
+*/
