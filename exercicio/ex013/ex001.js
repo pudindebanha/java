c5edbed20fc40b0b79fbc1b9b9e279311b41c187
pudin de banha <micapadua@hotmail.com>
2025-05-09 18:35:51 -0300

    a

--- exercicio/ex013/ex001.js
+++ exercicio/ex013/ex001.js
@@ -1,4 +1,4 @@
-for (let i = 1; i <= 10000; i++) {
+for (let i = 1; i <= 10; i++) {
     if(i%2===0){
         console.log(`${i} é par`)
     } else {
--- exercicio/ex013/node_71e616dd413fb.tmp.js
+++
@@ -1,7 +0,0 @@
-for (let i = 1; i <= 10000; i++) {
-    if(i%2===0){
-        console.log(`${i} é par`)
-    } else {
-        console.log(i + " é impar")
-    }
-}
