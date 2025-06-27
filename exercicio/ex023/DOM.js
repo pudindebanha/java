ea9a550ca3fe8fc516c04bcb66b2e6ea1fb7e2ab
pudin de banha <micapadua@hotmail.com>
2025-06-26 21:35:50 -0300

    ATT

---
+++ exercicio/ex023/DOM.js
@@ -0,0 +1,27 @@
+const caixa1 = document.getElementById("caixa1")
+const btn_c1 = document.querySelectorAll(".c1")
+
+
+console.log(btn_c1[0].getRootNode()) //Buscar o Nó Raiz
+console.log(caixa1.ownerDocument) //Buscar o Nó Raiz
+
+
+console.log(caixa1.children) //Busca os filhos da div Caixa
+console.log(caixa1.firstChild) //Busca o Primeiro filho da div Caixa
+console.log(caixa1.lastChild) //Busca o Ultimo filho da div Caixa
+
+
+console.log(caixa1.hasChildNodes()) //Busca saber se dentro da caixa div existem filhos! Resultado: True
+if(btn_c1[0].children.length > 0){
+    console.log("True") //Verifica se o btn children e maior que um, sendo assim mostra se tem mais elementos dentro da div btn_c1
+} else { console.log("False") }   
+console.log(btn_c1[0].children.length > 0 ? "Verdade" : "Falso") //Jeito mais simplificado
+
+
+console.log(caixa1.children[1].innerHTML = 'Troquei 1') //Altera o texto da segunda div dentro da div caixa
+console.log(caixa1.firstElementChild.innerHTML = 'Micael')//Altera o texto da primeira div dentro da div caixa
+console.log(caixa1.lastElementChild.innerHTML = 'MI-K')//Altera o texto da ultima div dentro da div caixa
+
+
+console.log(btn_c1[0].parentNode) //Busca o pai da div btn_c1, que no caso é a div caixa1
+console.log(btn_c1[0].parentNode.children[4])//Busca o pai da div btn_c1 (caixa1) e procura o filho[4] que no caso é (SQL)
