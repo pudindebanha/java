90fabf86ef1f84b3f2831500030137ccf7c044d2
pudin de banha <micapadua@hotmail.com>
2025-07-02 20:40:29 -0300

    att

--- exercicio/ex023/DOM.js
+++ exercicio/ex023/DOM.js
@@ -1,6 +1,12 @@
 const caixa1 = document.getElementById("caixa1")
 const btn_c1 = document.querySelectorAll(".c1")
 
+const novoElemento = document.createElement("div") //Cria um novo elemento DIV
+caixa1.appendChild(novoElemento) //Adiciona o novoElemento na div caixa1
+novoElemento.innerHTML = 'Micael' //Altera o texto do novoElemento
+novoElemento.setAttribute("id", "c7") //Adiciona atributos como id e o valor de id
+novoElemento.setAttribute("class", "curso c1") //Adiciona atributos como class e o valor da class
+
 
 console.log(btn_c1[0].getRootNode()) //Buscar o Nó Raiz
 console.log(caixa1.ownerDocument) //Buscar o Nó Raiz
---
+++ exercicio/ex023/DOMsemHTML.js
@@ -0,0 +1,19 @@
+const caixa1 = document.getElementById("caixa1")
+const btn_c1 = document.querySelectorAll(".c1")
+const cursos = ['HTML', 'CSS', 'JS', 'MYSQL', 'REACT']
+
+cursos.map((el,chave)=>{
+    const tot = chave + 1
+    const novoElemento = document.createElement("div")
+    caixa1.appendChild(novoElemento)
+    novoElemento.innerHTML = el
+    novoElemento.setAttribute ('id', 'c' + tot)
+    novoElemento.setAttribute ('class', 'curso c1')
+})
+
+
+const novoElemento = document.createElement("div") //Cria um novo elemento DIV
+caixa1.appendChild(novoElemento) //Adiciona o novoElemento na div caixa1
+novoElemento.innerHTML = 'Micael' //Altera o texto do novoElemento
+novoElemento.setAttribute("id", "c7") //Adiciona atributos como id e o valor de id
+novoElemento.setAttribute("class", "curso c1") //Adiciona atributos como class e o valor da class
