8890d52c794717d9bd6fd89538df54c7303826c6
pudin de banha <micapadua@hotmail.com>
2025-08-06 21:10:57 -0300

    att

---
+++ exercicio/ex025/find.js
@@ -0,0 +1,7 @@
+const p_array = document.getElementById("array")
+const input = document.getElementById("input")
+const btn_pesquisar = document.getElementById("btn_pesquisar")
+const resultado = document.getElementById("resultado")
+
+const elementos_array = [10,2,3,15,22,30]
+p_array.innerHTML = "["+elementos_array+"]"
