4ceba03989b8b3d52eb2b8d400a2acda63514319
pudin de banha <micapadua@hotmail.com>
2025-08-11 21:15:16 -0300

    att

--- exercicio/ex025/find.js
+++ exercicio/ex025/find.js
@@ -1,7 +1,19 @@
 const p_array = document.getElementById("array")
 const input = document.getElementById("input")
-const btn_pesquisar = document.getElementById("btn_pesquisar")
+const btnPesquisar = document.getElementById("btn_pesquisar")
 const resultado = document.getElementById("resultado")
 
-const elementos_array = [10,2,3,15,22,30]
+const elementos_array = [10,2,3,15,22,30,40]
 p_array.innerHTML = "["+elementos_array+"]"
+
+btnPesquisar.addEventListener("click", (evt)=>{
+    const ret =elementos_array.find((e,i)=>{
+        if(e === input.value) {
+            return true
+        }
+    })  
+    console.log(ret);
+    
+})
+
+
