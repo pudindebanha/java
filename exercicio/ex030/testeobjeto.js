4e931a81c4ba4f9cc55e1986e49c130d02c91fc6
pudin de banha <micapadua@hotmail.com>
2025-09-20 22:25:09 -0300

    att

---
+++ exercicio/ex030/testeobjeto.js
@@ -0,0 +1,5 @@
+btn_add = document.getElementById("btn_add")
+res = document.getElementById("box")
+inputnome = document.getElementById("inputnome")
+inputidade = document.getElementById("inputidade")
+
